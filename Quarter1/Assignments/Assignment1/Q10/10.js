e88bc02ddad6ee03ab82efaf110c6653db26bcb7
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:31:08 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q10/10.js
@@ -0,0 +1,10 @@
+"use strict";
+// Create message with name.
+let na_me = "Farhan";
+let message = "My name is ";
+console.log(message + na_me);
+//describes the  touppercase and tolowercase
+let namevar = " Shayan";
+console.log(namevar.toUpperCase());
+console.log(namevar.toLowerCase());
+console.log(namevar);
