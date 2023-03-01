c45b63f1cd3af9360cc17138a9a5291f8b7be0cb
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 14:08:27 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q34/34.js
@@ -0,0 +1,14 @@
+"use strict";
+let fav_pizza = ["chicken", "beef", "vegitable"];
+for (let index = 0; index < fav_pizza.length; index++) {
+    console.log(fav_pizza[index]);
+}
+for (let index = 0; index < fav_pizza.length; index++) {
+    console.log("I like " + fav_pizza[index] + " pizza");
+}
+let pizza1 = fav_pizza[0];
+let pizza2 = fav_pizza[1];
+let pizza3 = fav_pizza[2];
+console.log("I like " + pizza1 + " my favourite food is pizza. \n It is a famous Italian food. \n It is one of the favourite food specially among kids. ");
+console.log("I like " + pizza2 + " my favourite food is pizza. \n It is a famous Italian food. \n It is one of the favourite food specially among kids. ");
+console.log("I like " + pizza3 + " my favourite food is pizza. \n It is a famous Italian food. \n It is one of the favourite food specially among kids. ");
