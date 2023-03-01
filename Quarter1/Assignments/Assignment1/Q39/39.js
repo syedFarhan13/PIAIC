294855a630fb68c6012a6df1d80e61aaef2d9fbd
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 14:15:35 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q39/39.js
@@ -0,0 +1,7 @@
+"use strict";
+function city_country(city, country) {
+    console.log("\"" + city, country + "\"");
+}
+city_country("Lahore,", "Pakistan");
+city_country("sydney,", "Australia");
+city_country("Newyork,", "USA");
---
+++ Quarter1/Assignments/Assignment1/Q39/39.ts
@@ -0,0 +1,7 @@
+function city_country(city, country ) {
+    console.log("\""+city ,country +"\"" )
+}
+
+city_country("Lahore," ,"Pakistan");
+city_country("sydney," ,"Australia");
+city_country("Newyork,", "USA");
