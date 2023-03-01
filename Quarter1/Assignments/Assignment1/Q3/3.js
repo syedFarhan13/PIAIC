4578bd622565956f32926cbfe5cb26f895ead31a
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:09:42 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q3/3.js
@@ -0,0 +1,5 @@
+"use strict";
+let person = "Ali";
+console.log(person.toUpperCase);
+console.log(person.toLowerCase);
+console.log(person);
---
+++ Quarter1/Assignments/Assignment1/Q3/3.ts
@@ -0,0 +1,4 @@
+let person = "Ali";
+console.log(person.toUpperCase)
+console.log(person.toLowerCase)
+console.log(person)
