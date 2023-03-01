a3df86c264babb1b50ba8d53ce81f57540300df3
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:34:21 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q13/13.js
@@ -0,0 +1,5 @@
+"use strict";
+let fav_transportation = ["bike", "car", "bus"];
+for (let index = 0; index < fav_transportation.length; index++) {
+    console.log("i would like to own Honda " + fav_transportation[index]);
+}
