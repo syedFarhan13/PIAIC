7ba83902c7f9dcebc3bccfdfd00e9fcbf0d32ffd
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:36:40 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q14/14.ts
@@ -0,0 +1,28 @@
+let guest_list = ["Ali","Abdullah","Farhan","Shayan"]
+
+
+for (let index = 0; index < guest_list.length; index++) {
+    console.log ( "Hello " +guest_list[index]+" I would like to invite you at dinner."   ) ;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
