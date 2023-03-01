86fd3f8662f49d752b840aaeb5b7685c15d3b137
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:52:05 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q22/22.js
@@ -0,0 +1,4 @@
+"use strict";
+let myArray = [7, 8, 6, 8];
+console.log(myArray.length);
+console.log(myArray[6]);
---
+++ Quarter1/Assignments/Assignment1/Q22/22.ts
@@ -0,0 +1,5 @@
+let myArray = [7, 8, 6, 8];
+console.log(myArray.length);
+
+console.log(myArray[6]);
+
