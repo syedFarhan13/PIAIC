f8f02eae4cec10570daebaba31890c55b5e45830
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:56:12 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q25/25.ts
@@ -0,0 +1,8 @@
+let alien_color = "green";
+    
+if (alien_color == "green"){ 
+    console.log (" The player just earned 5 points"); 
+}
+if (alien_color  == "red"){ 
+    console.log (" The player just earned 7 points"); 
+}
