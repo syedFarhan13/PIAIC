5ee5fde50fcee355908735dd90082c4c45ee3936
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:40:43 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q15/15.ts
@@ -0,0 +1,38 @@
+let guest_list = ["Ali","Abdullah","Farhan","Shayan"]
+
+
+for (let index = 0; index < guest_list.length; index++) {
+    console.log ( "Hello " +guest_list[index]+" I would like to invite you at dinner."   ) ;
+    
+}
+
+// Abdullah is not comming
+console.log( "Abdullah is not not comming");
+
+//kumail is comming 
+guest_list[1] = "kumail"
+console.log ( guest_list);
+
+for (let index = 0; index < guest_list.length; index++) {
+    console.log ( "Hello " +guest_list[index]+" I would like to invite you at dinner.")
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
