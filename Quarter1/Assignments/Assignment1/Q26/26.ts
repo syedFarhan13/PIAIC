ef4b177350315e5d1ea80a780669321564c6419a
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:57:11 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q26/26.ts
@@ -0,0 +1,19 @@
+let alien_color = "green";
+// Version that runs the if block
+
+if (alien_color == "green") {console.log ( "the player just earned 5 points"); 
+    
+} else { console.log  ("the player just earned 10 points");
+    
+}
+
+    alien_color = "red"
+if (alien_color == "green") {console.log ( "the player just earned 5 points"); 
+    
+} else { console.log  ("the player just earned 10 points");
+    
+}
+
+
+
+
