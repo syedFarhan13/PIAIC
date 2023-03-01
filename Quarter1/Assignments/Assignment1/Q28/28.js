65df3ec09f66d8c8c74328a2e9e49075c08607cd
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 14:00:01 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q28/28.js
@@ -0,0 +1,17 @@
+"use strict";
+let person_age = 45;
+if (person_age < 2) {
+    console.log("the person is Baby");
+}
+else if (person_age >= 2 && person_age < 4) {
+    console.log("the person is toddler");
+}
+else if (person_age >= 4 && person_age < 13) {
+    console.log(" the person is a kid");
+}
+else if (person_age >= 13 && person_age < 20) {
+    console.log(" the person is a teenager");
+}
+else if (person_age >= 20 && person_age < 65) {
+    console.log(" the person is an adult");
+}
