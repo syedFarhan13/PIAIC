2a3e5968f40c81575e0169bca8d5a78155e85583
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 14:05:55 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q33/33.ts
@@ -0,0 +1,31 @@
+// Store the numbeers 1 through 9 in array.
+
+let the_num=[1,2,3,4,5,6,7,8,9];
+let nums
+
+for (let index = 0; index < the_num.length; index++) {
+    
+    nums= the_num[index]
+   if (nums == 1 ) {console.log(nums +"st")
+    
+   } 
+   else if(nums == 2){ console.log (nums + "nd")}
+   else if (nums == 3) { console.log (nums + "rd" )}
+   else if (nums == 4) { console.log (nums + "rd")}
+   else if(nums == 5){ console.log (nums + "th")}
+   else if (nums == 6) { console.log (nums + "th" )}
+   else if (nums == 7) { console.log (nums + "th")}
+   else if(nums == 8){ console.log (nums + "th")}
+   else if (nums == 9) { console.log (nums + "th" )}
+   
+
+
+    
+}
+    
+
+
+
+
+
+
