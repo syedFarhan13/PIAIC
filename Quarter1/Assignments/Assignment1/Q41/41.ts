e7063a932edeac876a6585a1011f20c2aa8b8ec0
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 14:18:29 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q41/41.ts
@@ -0,0 +1,17 @@
+
+let magicians_names = ["MAX MAVEN", "MASKED MAGICIAN","JAY MARSHALL"];
+function show_magicians(magicians) {
+    for (let index = 0; index < magicians_names.length; index++) {
+        console.log (magicians_names[index]);
+        
+    }
+    
+}
+
+
+ show_magicians (magicians_names)
+
+
+
+
+
