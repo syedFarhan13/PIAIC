be91bb8ec68f5c4193ec47cf1ea5516a5e0544de
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:52:56 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q23/23.ts
@@ -0,0 +1,37 @@
+let gasBill = 1200;
+let wapdaBill = 5000;
+let vehicle = "car";
+let currency = "pkr";
+let mobile = "iPhone";
+
+// 5 true statements
+console.log("Is gasBill >= 1000 ? I predict True")
+console.log(gasBill >= 1000)
+
+console.log("Is wapdaBill <= 6000 ? I predict True")
+console.log(wapdaBill <= 6000)
+
+console.log("Is vehicle == 'car' ? I predict True")
+console.log(vehicle == "car")
+
+console.log("Is currency == 'pkr' ? I predict True")
+console.log(currency == "pkr")
+
+console.log("Is mobile == 'iPhone' ? I predict True")
+console.log(mobile == "iPhone")
+
+// 5 false statements
+console.log("Is gasBill == 1000 ? I predict False")
+console.log(gasBill == 1000)
+
+console.log("Is wapdaBill == 6000 ? I predict False")
+console.log(wapdaBill == 6000)
+
+console.log("Is vehicle == 'bus' ? I predict False")
+console.log(vehicle == "bus")
+
+console.log("Is currency == 'dollar' ? I predict False")
+console.log(currency == "dollar")
+
+console.log("Is mobile == 'Samsung' ? I predict False")
+console.log(mobile == "Samsung")
