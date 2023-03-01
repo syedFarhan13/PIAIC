f5150e770946e1224b31cc72ebf409edcd874d5a
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:53:59 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q24/24.js
@@ -0,0 +1,26 @@
+"use strict";
+// Tests for equality and inequality with strings
+console.log("hello" == "hello"); // true
+console.log("hello" == "world"); // false
+console.log("hello" != "world"); // true
+// Tests using the toLowerCase() function
+console.log("HELLO".toLowerCase() == "hello"); // true
+console.log("hello".toLowerCase() == "world"); // false
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+console.log(5 == 5); // true
+console.log(5 == 10); // false
+console.log(5 != 10); // true
+console.log(5 > 3); // true
+console.log(5 < 3); // false
+console.log(5 >= 5); // true
+console.log(5 <= 3); // false
+// Tests using "and" and "or" operators
+console.log((5 > 3) && (10 > 5)); // true
+console.log((5 > 10) || (10 > 5)); // true
+// Test whether an item is in an array
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers.includes(3)); // true
+console.log(numbers.includes(10)); // false
+// Test whether an item is not in an array
+console.log(!numbers.includes(3)); // false
+console.log(!numbers.includes(10)); // true
