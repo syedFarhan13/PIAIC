f58f4943fa1155fdd5596acfb16134d14faf36ea
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 14:00:56 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q29/29.ts
@@ -0,0 +1,32 @@
+//Make a array of your three favorite fruits and call it favorite_fruits
+let favourite_fruits = ["apple", "banana", "mango"];
+
+if (favourite_fruits.includes("banana")) {
+    console.log("you reallly like banana!");
+    }
+
+if (favourite_fruits.includes("apple")) {
+        console.log("you really like apple");
+        
+    }
+
+if (favourite_fruits.includes("orange")) {
+    console.log("you really like orange");
+        
+    }
+
+if (favourite_fruits.includes("mango")) {
+    console.log("you really like mango");
+    
+}
+
+if (favourite_fruits.includes("gauva")) {
+    console.log("you really like gauva");
+    
+}
+
+
+
+
+
+
