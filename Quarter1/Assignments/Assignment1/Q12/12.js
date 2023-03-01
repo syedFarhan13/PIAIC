bf1dcb54d93f1eea94877455fc93a000abd76f9a
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:33:16 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q12/12.js
@@ -0,0 +1,5 @@
+"use strict";
+let friend_name = ["Ali", "Farhan", "Shayan", "Hani"];
+for (let index = 0; index < friend_name.length; index++) {
+    console.log("Hello " + friend_name[index] + " How are You");
+}
