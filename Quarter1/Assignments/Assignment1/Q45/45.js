08341514dc7f1634336ecb223ea43aff1b90d11d
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 14:22:59 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q45/45.js
@@ -0,0 +1,15 @@
+"use strict";
+function createCar(manufacturer, model, ...options) {
+    let car = {
+        manufacturer: manufacturer,
+        model: model
+    };
+    for (let i = 0; i < options.length; i += 2) {
+        let key = options[i];
+        let value = options[i + 1];
+        car[key] = value;
+    }
+    return car;
+}
+let myCar = createCar("Tesla", "Model S", "color", "red", "autopilot", true);
+console.log(myCar);
