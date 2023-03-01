a8b7ef20ebae5702ce4cae609e0b465ba5bb0615
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:46:02 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q20/20.js
@@ -0,0 +1,7 @@
+"use strict";
+//Think of something you could store in a array.
+let myListOfCountries = ["Pakistan", "India", "Iran", "Afghanistan", "China"];
+console.log(myListOfCountries);
+console.log(myListOfCountries[0]);
+console.log(myListOfCountries[3]);
+console.log(myListOfCountries.length);
