29eb45e3b133d4e4ace4a9d542eb3fbf2ba3c4ce
syedFarhan13 <116066548+syedFarhan13@users.noreply.github.com>
2023-03-01 13:45:06 +0500

    Add files via upload

---
+++ Quarter1/Assignments/Assignment1/Q19/19.js
@@ -0,0 +1,7 @@
+"use strict";
+let dinner_guest = ["nimmy", "timmy", "gimmy"];
+let numguest = dinner_guest.length;
+for (let i = 0; i < numguest; i++) {
+    console.log("Hello " + dinner_guest[i] + " I would like u to inviting at dinner");
+}
+console.log(numguest + " people are invited");
